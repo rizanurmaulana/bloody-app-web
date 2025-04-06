02e37ad32adcb5eeac4a56dc816a0835bba88380
rizanurmaulana <rizanurmaulana2810@gmail.com>
2025-04-06 22:58:43 +0700

    feat: update user table and improve datatable

--- src/components/datatable/DataTableBody.tsx
+++ src/components/datatable/DataTableBody.tsx
@@ -1,23 +1,24 @@
 import { Table } from '@tanstack/react-table';
 import { flexRender } from '@tanstack/react-table';
+import { TableCell, TableRow } from '../ui/table';
 
 export default function DataTableBody({ table }: { table: Table<any> }) {
   return (
     <>
       {table.getRowModel().rows.map((row) => (
-        <tr
+        <TableRow
           key={row.id}
           className='divide-y divide-gray-100 dark:divide-white/[0.05]'
         >
           {row.getVisibleCells().map((cell) => (
-            <td
+            <TableCell
               key={cell.id}
               className='px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400'
             >
               {flexRender(cell.column.columnDef.cell, cell.getContext())}
-            </td>
+            </TableCell>
           ))}
-        </tr>
+        </TableRow>
       ))}
     </>
   );
